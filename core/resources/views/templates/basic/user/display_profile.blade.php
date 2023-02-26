3cde0d6ed0f02fb10621c1b8dc9598b8d6d3b83a
mrorko840 <mrorko840@gmail.com>
2023-02-26 16:58:36 +0600

    fix

--- core/resources/views/templates/basic/user/display_profile.blade.php
+++ core/resources/views/templates/basic/user/display_profile.blade.php
@@ -551,33 +551,33 @@
         });
 
         //cover-photo Upload//
-        // $(".coverPicUpload").on('change', function(e) {
-        //     e.preventDefault();
-        //     // $('#coverPhotoSpin').html(`
-        //     //     <div class="spinner-border spinner-border-sm mt-1" role="status">
-        //     //         <span class="visually-hidden"></span>
-        //     //     </div>
-        //     //     `);
-        //     $.ajax({
-        //         method: "POST",
-        //         url: "{{route('user.cover.photo')}}",
-        //         data: new FormData($("#coverImgForm")[0]),
-        //         processData: false,
-        //         dataType: 'json',
-        //         contentType: false,
-        //         success: function (res) {
-        //             console.log(res.msg);
-        //             // $('#coverPhotoSpin').html(`
-        //             //     <span  class="material-icons">photo_camera</span>
-        //             // `);
-        //             notifyMsg(res.msg,res.cls);
-        //             $('.coverPhoto').attr('style', '');
-        //             $('.coverPhoto').attr('style', "background-image: url('{{route('home')}}/"+res.img+"');");
+        $(".coverPicUpload").on('change', function(e) {
+            e.preventDefault();
+            $('#coverPhotoSpin').html(`
+                <div class="spinner-border spinner-border-sm mt-1" role="status">
+                    <span class="visually-hidden"></span>
+                </div>
+                `);
+            $.ajax({
+                method: "POST",
+                url: "{{route('user.cover.photo')}}",
+                data: new FormData($("#coverImgForm")[0]),
+                processData: false,
+                dataType: 'json',
+                contentType: false,
+                success: function (res) {
+                    console.log(res.msg);
+                    $('#coverPhotoSpin').html(`
+                        <span  class="material-icons">photo_camera</span>
+                    `);
+                    notifyMsg(res.msg,res.cls);
+                    $('.coverPhoto').attr('style', '');
+                    $('.coverPhoto').attr('style', "background-image: url('{{route('home')}}/"+res.img+"');");
 
-        //         }
-        //     });
+                }
+            });
 
-        // });
+        });
     </script>
 @endpush
 @endsection
